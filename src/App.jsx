239dff2ebc4e86040d51bf8df6fe8b30d3eff086
malika <malika727@gmail.com>
2024-08-04 23:04:09 +0530

    initialize commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-aaa
+
+const App = () => {
+
+
+  return (
+      <>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </>
+  )
+}
+
+export default App
